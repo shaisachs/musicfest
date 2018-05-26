33dbab9d1d5ee3a23b79d55aca23f5a0490e40e8
Shai Sachs <shaisachs@gmail.com>
2018-05-26 12:49:36 -0400

    Refactor to use decorator pattern

--- scrape.py
+++ scrape.py
@@ -10,6 +10,96 @@ def canonicaltime(text):
     return hour + 12
 
 
+def performerDecorator(cells, resource):
+    band = {}
+
+    imageCell = cells[0]
+    bandInfoCell = cells[1]
+    genresCell = cells[2]
+
+    if bandInfoCell and bandInfoCell.a and bandInfoCell.a.string:
+        band['name'] = bandInfoCell.a.string
+
+    if bandInfoCell and bandInfoCell.a and bandInfoCell.a['href']:
+        bandUrl = bandInfoCell.a['href']
+
+        if bandUrl.startswith('/node'):
+            bandUrl = 'https://www.somervilleartscouncil.org' + bandUrl
+
+        band['url'] = bandUrl
+
+    if imageCell and imageCell.a and imageCell.a.img:
+        band['image'] = imageCell.a.img['src']
+
+    if band and genresCell:
+        genres = []
+        for genreLink in genresCell.findAll('a'):
+            if genreLink and genreLink.string:
+                genres.append(genreLink.string)
+
+        band['genre'] = genres
+
+    if band:
+        band['@type'] = 'MusicGroup'
+        resource['performer'] = band
+
+    return resource
+
+def locationDecorator(cells, resource):
+    venue = {}
+
+    addressCell = cells[3].find(class_='street-address')
+
+    if addressCell and addressCell.string:
+        venue['@type'] = 'Place'
+        venue['address'] = {}
+        venue['address']['@type'] = 'PostalAddress'
+        venue['address']['streetAddress'] = addressCell.string.strip()
+        venue['address']['addressLocality'] = 'Somerville'
+        venue['address']['addressRegion'] = 'MA'
+        venue['address']['addressCountry'] = 'US'
+
+    if venue:
+        resource['location'] = venue
+
+    return resource
+
+def timeDecorator(cells, resource):
+    timeCell = cells[4]
+
+    if timeCell:
+        startTimeDiv = timeCell.find(class_='date-display-start')
+        if startTimeDiv and startTimeDiv.string:
+            startHour = canonicaltime(startTimeDiv.string)
+            resource['startDate'] = '2018-05-13T' + str(startHour) + ':00:00-04:00'
+
+    if timeCell:
+        endTimeDiv = timeCell.find(class_='date-display-end')
+        if endTimeDiv and endTimeDiv.string:
+            endHour = canonicaltime(endTimeDiv.string)
+            if endHour:
+                resource['endDate'] = '2018-05-13T' + str(endHour) + ':00:00-04:00'
+
+    return resource
+
+
+def eventDecorator(cells, resource):
+    resource = {}
+
+    resource = performerDecorator(cells, resource)
+    resource = locationDecorator(cells, resource)
+    resource = timeDecorator(cells, resource)
+
+    if resource['performer'] and resource['performer']['name']:
+        resource['name'] = resource['performer']['name']
+
+    if resource:
+        resource['@context'] = 'http://schema.org/'
+        resource['@type'] = 'Event'
+
+    return resource
+
+
 mapurl = 'https://www.somervilleartscouncil.org/porchfest/map/2018'
 page = requests.get(mapurl)
 parsedpage = BeautifulSoup(page.content, 'html.parser')
@@ -32,69 +122,12 @@ for row in tablebody.findAll('tr'):
     if not cells:
         continue
 
-    imageCell = cells[0]
-    bandInfoCell = cells[1]
-    genresCell = cells[2]
-    addressCell = cells[3].find(class_='street-address')
-    timeCell = cells[4]
+    show = {}
 
-    band = {}
+    show = eventDecorator(cells, show)
 
-    if bandInfoCell and bandInfoCell.a and bandInfoCell.a.string:
-        band['name'] = bandInfoCell.a.string
-
-    if bandInfoCell and bandInfoCell.a and bandInfoCell.a['href']:
-        bandUrl = bandInfoCell.a['href']
-
-        if bandUrl.startswith('/node'):
-            bandUrl = 'https://www.somervilleartscouncil.org' + bandUrl
-
-        band['url'] = bandUrl
-
-    if imageCell and imageCell.a and imageCell.a.img:
-        band['image'] = imageCell.a.img['src']
-
-    if band:
-        band['@type'] = 'MusicGroup'
-        show['performer'] = band
-        show['name'] = band['name']
-
-    if addressCell and addressCell.string:
-        venue = {}
-        venue['@type'] = 'Place'
-        venue['address'] = {}
-        venue['address']['@type'] = 'PostalAddress'
-        venue['address']['streetAddress'] = addressCell.string.strip()
-        venue['address']['addressLocality'] = 'Somerville'
-        venue['address']['addressRegion'] = 'MA'
-        venue['address']['addressCountry'] = 'US'
-        show['location'] = venue
-
-    if band and genresCell:
-        genres = []
-        for genreLink in genresCell.findAll('a'):
-            if genreLink and genreLink.string:
-                genres.append(genreLink.string)
-
-        show['performer']['genre'] = genres
-
-    if timeCell:
-        startTimeDiv = timeCell.find(class_='date-display-start')
-        if startTimeDiv and startTimeDiv.string:
-            startHour = canonicaltime(startTimeDiv.string)
-            show['startDate'] = '2018-05-13T' + str(startHour) + ':00:00-04:00'
-
-    if timeCell:
-        endTimeDiv = timeCell.find(class_='date-display-end')
-        if endTimeDiv and endTimeDiv.string:
-            endHour = canonicaltime(endTimeDiv.string)
-            if endHour:
-                show['endDate'] = '2018-05-13T' + str(endHour) + ':00:00-04:00'
-
-    show['@context'] = 'http://schema.org/'
-    show['@type'] = 'Event'
-
-    shows.append(show)
+    if show:
+        shows.append(show)
 
 output = {}
 output['shows'] = shows
